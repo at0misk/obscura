bc692bc29889201e564049f86e88b97290281067
Chris Lee <chris.leeau@me.com>
2016-03-29 14:31:58 -0700

    .

---
+++ application/controllers/Products.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Products extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('index');
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
